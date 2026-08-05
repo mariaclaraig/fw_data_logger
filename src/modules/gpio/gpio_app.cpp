16a8345a80344cb82799a62891be94e8b0bf4cd3
mariaclaraig <mclara.ignacio@gmail.com>
2026-08-05 09:55:26 -0300

    refactor: remove arquivos gpio redundantes

--- src/modules/gpio/gpio_app.cpp
+++ src/modules/gpio/gpio_app.cpp
@@ -1,18 +1,21 @@
 #include "modules/gpio/gpio_app.h"
 
-#include "modules/gpio/actuator/gpio_actuator.h"
+#include <Arduino.h>
+
+#include "project_config.h"
 
 void gpio_app_init()
 {
-    gpio_actuator_init();
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
 }
 
 void gpio_app_status_led_off()
 {
-    gpio_actuator_status_led_off();
+    digitalWrite(LED_PIN, LED_OFF);
 }
 
 void gpio_app_status_led_toggle()
 {
-    gpio_actuator_status_led_toggle();
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
 }
